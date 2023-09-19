642a2b880cc4264f7afd9b8f8bfa209fb208c611
Donavan Marques <donavan.fm@hotmail.com>
2023-09-18 21:47:55 -0300

    teste

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-	baseURL: "https://us-central1-principal-storm-395723.cloudfunctions.net/",
+	baseURL: "https://us-central1-principal-storm-395723.cloudfunctions.net",
 });
 
 export default Api;
